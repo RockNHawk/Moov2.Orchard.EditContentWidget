c9b08f4b5b29067e3073143a7dd14c38403801b3
mobinzk <mobinzk@gmail.com>
2017-10-10 18:01:12 +0100

    Add migration

---
+++ Migrations.cs
@@ -0,0 +1,24 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+using Orchard.Environment.Extensions;
+
+namespace Asmodee.Orchard.EditContentWidget
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("EditContentWidget",
+                cfg => cfg
+                    .WithPart("EditContentWidgetPart")
+                    .WithPart("CommonPart")
+                    .WithPart("WidgetPart")
+                    .WithSetting("Stereotype", "Widget")
+                    .WithIdentity()
+                );
+
+            return 1;
+        }
+    }
+}
